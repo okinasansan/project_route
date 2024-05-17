a6d640ce9a1f403470fb9bb6e9bb81ddddb8b5aa
Okina <nao.xsrz2@gmail.com>
2024-05-17 20:35:36 +0900

    second commit

---
+++ project_route/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "projectroute": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
