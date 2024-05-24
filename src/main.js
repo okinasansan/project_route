bd4dca276387029bf84135437f5f7fc3a3f6f30b
Okina <nao.xsrz2@gmail.com>
2024-05-24 19:40:37 +0900

    05241940

--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 
-createApp(App).mount('#app')
+import Amplify, * as AmplifyModules from 'aws-amplify' // 追記
+import { AmplifyPlugin } from 'aws-amplify-vue' // 追記
+import aws_exports from './aws-exports' // 追記
+Amplify.configure(aws_exports) // 追記
+
+Vue.use(AmplifyPlugin, AmplifyModules) // 追記
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
