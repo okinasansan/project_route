5c25e907d6c3bfc97b8188271b63d748d2ee4437
okinasansan <nao.xsrz2@gmail.com>
2024-06-14 20:00:04 +0900

    よけいなもの消去

--- src/main.js
+++ src/main.js
@@ -5,16 +5,6 @@ import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 import '@mdi/font/css/materialdesignicons.css'
 
-import Amplify, * as AmplifyModules from 'aws-amplify' // 追記
-import { AmplifyPlugin } from 'aws-amplify-vue' // 追記
-import aws_exports from './aws-exports' // 追記
-Amplify.configure(aws_exports) // 追記
-
-Vue.use(AmplifyPlugin, AmplifyModules) // 追記
-
-Vue.config.productionTip = false
-
-
 loadFonts()
 
 createApp(App)
