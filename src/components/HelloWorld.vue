fdcf3d061f17216fb03079a982556bf9514ebfc5
okinasansan <nao.xsrz2@gmail.com>
2024-06-28 20:39:34 +0900

    検索結果テンプレートの作成（仮）

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,53 +11,43 @@
 
   <div> text: {{ finish_text }} </div>
 
-
-
-  <v-card
-    :loading="loading"
+  <v-container>
+    <v-card
     class="mx-auto my-12"
     max-width="800"
-  >
-    <template slot="progress">
-      <v-progress-linear
-        color="deep-purple"
-        height="10"
-        indeterminate
-      ></v-progress-linear>
-    </template>
+    >
+      <v-card-item v-for="item in items" v-bind:key="item.id">
+        <v-img
+        height="250"
+        src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+        ></v-img>
 
-    <v-img
-      height="250"
-      src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
-    ></v-img>
+        <v-card-title>{{ item.spot }}</v-card-title>
 
-    <v-card-title>Cafe Badilico</v-card-title>
+        <v-checkbox v-model="selected" value="1" label="Hoge1">
+        </v-checkbox>
 
-    <v-card-text>
-      <v-row
-        align="center"
-        class="mx-0"
-      >
-      </v-row>
+        <v-card-text>
+          <v-row
+            align="center"
+            class="mx-0"
+          >
+          </v-row>
 
-      <div class="my-4 text-subtitle-1">
-        $ • Italian, Cafe
-      </div>
+          <div class="my-4 text-subtitle-1">
+            {{ item.title }}
+          </div>
 
-      <div>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.</div>
-    </v-card-text>
+        </v-card-text>
 
-    <v-divider class="mx-4"></v-divider>
+        <v-divider class="mx-4"></v-divider>
 
-    <v-card-title>Tonight's availability</v-card-title>
-
-
-    <v-card-actions>
-      <v-btn>
-        Reserve
-      </v-btn>
-    </v-card-actions>
-  </v-card>
+        <v-card-title>{{ item.address }}</v-card-title>
+      </v-card-item>
+      
+    </v-card>
+  </v-container>
+  
 </template>
 
 <script>
@@ -65,12 +55,35 @@
 export default {
   name: 'HelloWorld',
 
-  data: () => ({
-      loading: false,
-      selection: 1,
-      text: '',
-      finish_text: '',
-    }),
+  data (){
+      //selection: 1,      
+
+      return {
+        text: '',
+        finish_text: '',
+        selected:[],
+        items:[
+          {
+            id: 1,
+            spot: "東京電機大学",
+            title: "でんだい",
+            address: "足立区",
+          },
+          {
+            id: 2,
+            spot: "ラジ館",
+            title: "シュタゲ",
+            address: "アキバ",
+          },
+          {
+            id: 3,
+            spot: "神社",
+            title: "シュタゲ",
+            address: "神田",
+          }
+        ]
+      }
+    },
   
   methods: {
     reserve () {
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -13,3 +13,8 @@
   }
 }
 </style>
+
+<script>
+
+</script>
+
